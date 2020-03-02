12ec999b2ff77af93f28e676bb5e6f0f7d88c1c6
sunny-sk <sk956223@gmail.com>
2020-03-02 23:48:11 +0530

    add task functionalty added

--- store/action/TaskActions.js
+++ store/action/TaskActions.js
@@ -1,13 +1,17 @@
 export const ADD_TASK = 'ADD_TASK';
 export const UPDATE_TASK = 'UPDATE_TASK';
 export const DELETE_TASK = 'DELETE_TASK';
+export const COMPLETE_TASK = 'COMPLETE_TASK';
 
 export const addTask = task => {
   return {type: ADD_TASK, task: task};
 };
 export const updateTask = task => {
-  return {type: ADD_TASK, task: task};
+  return {type: UPDATE_TASK, task: task};
+};
+export const completeTask = id => {
+  return {type: COMPLETE_TASK, id: id};
 };
 export const deleteTask = id => {
-  return {type: ADD_TASK, task: id};
+  return {type: DELETE_TASK, task: id};
 };
